41dce0924d9d3a338adf22219e010bbd735a67b0
Mattia Peiretti <mattia.peiretti@gmail.com>
2020-04-20 09:45:10 +0200

    added custom sync load json function with jquery

---
+++ functions.js
@@ -0,0 +1,12 @@
+function loadJson(url) {
+    let result;
+    $.ajax({
+        url: url,
+        dataType: "json",
+        async: false,
+        success: function (data) {
+            result = data;
+        },
+    });
+    return result;
+}
