d113c56987cacb94258d678b4ebcc678b6c8d160
Mattia Peiretti <mattia.peiretti@gmail.com>
2020-04-20 09:48:35 +0200

    Added new json lloading system that works syncronously

--- assets.js
+++ assets.js
@@ -17,69 +17,66 @@ class Texturepack {
     }
 
     load() {
-        this.readData();
-    }
+        let jsonData;
 
-    readData() {
-        DEBUG && console.log(`[${this.debugName}] Loading TexturePack`);
+        jsonData = loadJson(
+            `${TEXTUREPACK_BASE_DIR}/${this.friendlyName}/data.json`
+        );
 
-        //Fetching the info .json file located in the texturepack folder
-        fetch(`${TEXTUREPACK_BASE_DIR}/${this.friendlyName}/data.json`)
-            .then((res) => res.json())
-            .then((json) => {
-                //Processing the actual data
-                this.processData(json);
-            })
-            .catch(
-                (err) =>
-                    DEBUG &&
-                    console.error(
-                        `[${this.debugName}] An error occurred while loading the texturepack data: ${err}`
-                    )
-            );
-    }
-
-    processData(data) {
-        //The script contains the specs of the texturepack itself but also a complete dict of all the textures: BlockName : Id
-        DEBUG && console.log(`[${this.debugName}] Processing TexturePack Data`);
-        this.name = data.info.name;
-        this.assets = data.assets;
-
-        //Load the images into a dict
-        this.loadTextures();
-    }
-
-    loadTextures() {
-        //Loop trough the assets dict
-        this.undefinedTexture = loadImage(UNDEFINED_TEXTURE_PATH);
-
-        for (var block in this.assets) {
-            //Loading the actual images to the this.textures dict
-            this.textures[`${block}_texture`] = loadImage(
-                `${TEXTUREPACK_BASE_DIR}/${this.friendlyName}/${this.assets[block]}.png`
-
-                //Callbacks are bugged...
-                // //Callback in case of success
-                // ,(loadedImg) => {
-                //     DEBUG &&
-                //         console.log(
-                //             `[${this.debugName}] Loaded: ${block} texture with id: ${this.assets[block]} successfully!`
-                //         );
-                // },
-
-                // // callback in case of faliure
-                // () => {
-                //     DEBUG &&
-                //         console.error(
-                //             `[${this.debugName}] An error occurred while loading the texture: ${block} texture with id: ${this.assets[block]}, with filename: ${this.assets[block]}.png`
-                //         );
-                // }
-            );
-
-            DEBUG &&
-                console.log(
-                    `[${this.debugName}] Loaded: ${block} texture with id: ${this.assets[block]} successfully!`
-                );
+        if (jsonData) {
+            this.name = jsonData.info.name;
+            this.assets = jsonData.assets;
         }
+
+        if (this.assets) {
+            this.undefinedTexture = loadImage(UNDEFINED_TEXTURE_PATH);
+
+            for (var block in this.assets) {
+                //Loading the actual images to the this.textures dict
+                this.textures[`${block}_texture`] = loadImage(
+                    `${TEXTUREPACK_BASE_DIR}/${this.friendlyName}/${this.assets[block]}.png`
+
+                    //Callbacks are bugged...
+                    // //Callback in case of success
+                    // ,(loadedImg) => {
+                    //     DEBUG &&
+                    //         console.log(
+                    //             `[${this.debugName}] Loaded: ${block} texture with id: ${this.assets[block]} successfully!`
+                    //         );
+                    // },
+
+                    // // callback in case of faliure
+                    // () => {
+                    //     DEBUG &&
+                    //         console.error(
+                    //             `[${this.debugName}] An error occurred while loading the texture: ${block} texture with id: ${this.assets[block]}, with filename: ${this.assets[block]}.png`
+                    //         );
+                    // }
+                );
+
+                DEBUG &&
+                    console.log(
+                        `[${this.debugName}] Loaded: ${block} texture with id: ${this.assets[block]} successfully!`
+                    );
+            }
+        }
+    }
+}
+
+//Default block Class
+
+class Block {
+    constructor(texture, pos, size) {
+        this.tile = texture;
+        this.xx = pos[0];
+        this.yy = pos[1];
+        this.ww = size[0];
+        this.hh = size[1];
+    }
+
+    Draw() {
+        noSmooth();
+        image(this.tile, this.xx, this.yy, this.ww, this.hh);
+        smooth();
     }
 }
